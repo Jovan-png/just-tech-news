3e0b3759af9c60280aaf1b35ac4bf5c5705eb8a0
Jovan-png <88255194+Jovan-png@users.noreply.github.com>
2021-11-17 15:49:06 -0600

    added bcrypt functionality

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
+const bcrypt = require('bcrypt');
+const { beforeUpdate } = require('../config/connection');
 // create our User model
 class User extends Model {}
 
@@ -39,6 +40,19 @@ User.init(
     freezeTableName: true,
     underscored: true,
     modelName: 'user'
+  },
+  {
+  hooks: {
+      async beforeCreate(newUserData){
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+      },
+      async beforeUpdate(updatedUserData){
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+      }
+  }
+  
   }
 );
 
